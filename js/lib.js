9868e5136c9bc75906146bf3c4b08a60c5e5d0b9
SirQuandale <302685723@student.rocmondriaan.nl>
2022-11-22 14:23:28 +0100

    added a point system & fixed some bugs

--- js/lib.js
+++ js/lib.js
@@ -1,55 +1,69 @@
 function checkWinner() {
     for (let i = 0; i < 3; i++) {
-        if (gridBoard[i][0] == gridBoard[i][1] && gridBoard[i][1] == gridBoard[i][2] && gridBoard[i][2] != null) {
-            console.log('win');
-            let row = document.querySelectorAll(`.row${i + 1}`);
-            row.forEach(column => {
-                column.classList.add('won');
+        if (gridBoard[i][0] == gridBoard[i][1] && gridBoard[i][1] == gridBoard[i][2] && gridBoard[i][2] != null) { //checks if one of these rows is filled. the [0] or [1] = rows
+            didWin = true; //enables win
+            let row = document.querySelectorAll(`.row${i + 1}`); //checks all the three rows
+            row.forEach(column => { //for one of the winning rows, the background changes color to green.
+                column.classList.add('won'); //adds a green background to the winning 3 boxes
+                column.classList.remove('boxes-hover'); //removes the gray hover color
             })
-            addPoints();
+            addPoints(); //adds points to player or enemy. could also be a draw
             return gridBoard[i][0];
-        }
-
-        if (gridBoard[0][i] == gridBoard[1][i] && gridBoard[1][i] == gridBoard[2][i] && gridBoard[2][i] != null) {
-            console.log('win');
-
-            let column = document.querySelectorAll(`.column${i + 1}`)
+        } else if (gridBoard[0][i] == gridBoard[1][i] && gridBoard[1][i] == gridBoard[2][i] && gridBoard[2][i] != null) { //the same is repeated here, except the indexes are turned around to recognize horizontal rows
+            didWin = true;
+            let column = document.querySelectorAll(`.column${i + 1}`) //column instead of row
             column.forEach(columnrow => {
                 columnrow.classList.add('won');
+                columnrow.classList.remove('boxes-hover');
             })
-
             return gridBoard[i][0];
-        }
-
-        if (gridBoard[0][0] == gridBoard[1][1] && gridBoard[1][1] == gridBoard[2][2] && gridBoard[2][2] != null) {
-            console.log('win');
-
+        } else if (gridBoard[0][0] == gridBoard[1][1] && gridBoard[1][1] == gridBoard[2][2] && gridBoard[2][2] != null) { //specific indexes for diagnal win
+            didWin = true;
             let diagnl = document.querySelectorAll(`.diag1`)
             diagnl.forEach(diagrow => {
                 diagrow.classList.add('won');
+                diagrow.classList.remove('boxes-hover');
             })
-
             return gridBoard[0][0];
-        }
-
-        if (gridBoard[0][2] == gridBoard[1][1] && gridBoard[1][1] == gridBoard[2][0] && gridBoard[2][0] != null) {
-            console.log('win');
-
+        } else if (gridBoard[0][2] == gridBoard[1][1] && gridBoard[1][1] == gridBoard[2][0] && gridBoard[2][0] != null) { //diagnal win
+            didWin = true;
+            console.log(didWin);
             let diagnl2 = document.querySelectorAll(`.diag2`)
             diagnl2.forEach(diagrow2 => {
                 diagrow2.classList.add('won');
+                diagrow2.classList.remove('boxes-hover');
             })
-
             return gridBoard[0][0];
         }
-
     }
 }
 
 function addPoints() {
     for (let i = 0; i < 3; i++) {
-        if (gridBoard[i][0] == gridBoard[i][1] && gridBoard[i][1] == gridBoard[i][2] && gridBoard[i][2] != null) {
-            console.log('added point');
+        if (gridBoard[i][0] == gridBoard[i][1] && gridBoard[i][1] == gridBoard[i][2] && gridBoard[i][2] == 'X') {
+            totalWinsX = totalWinsX + 1;
+            console.log(totalWinsX + 'X');
+        } else if (gridBoard[i][0] == gridBoard[i][1] && gridBoard[i][1] == gridBoard[i][2] && gridBoard[i][2] == 'O') {
+            totalWInsO = totalWInsO + 1;
+            console.log(totalWInsO + 'O');
+        } else if (gridBoard[0][i] == gridBoard[1][i] && gridBoard[1][i] == gridBoard[2][i] && gridBoard[2][i] == 'X') {
+            totalWinsX = totalWinsX + 1;
+            console.log(totalWinsX + 'X');
+        } else if (gridBoard[0][i] == gridBoard[1][i] && gridBoard[1][i] == gridBoard[2][i] && gridBoard[2][i] == 'O') {
+            totalWInsO = totalWInsO + 1;
+            console.log(totalWInsO + 'O');
+        } else if (gridBoard[0][0] == gridBoard[1][1] && gridBoard[1][1] == gridBoard[2][2] && gridBoard[2][2] == 'X') {
+            totalWinsX = totalWinsX + 1;
+            console.log(totalWinsX + 'X');
+        } else if (gridBoard[0][0] == gridBoard[1][1] && gridBoard[1][1] == gridBoard[2][2] && gridBoard[2][2] == 'O') {
+            totalWInsO = totalWInsO + 1;
+            console.log(totalWInsO + 'O');
+        } else if (gridBoard[0][2] == gridBoard[1][1] && gridBoard[1][1] == gridBoard[2][0] && gridBoard[2][0] == 'X') {
+            totalWinsX = totalWinsX + 1;
+            console.log(totalWinsX + 'X');
+        } else if (gridBoard[0][2] == gridBoard[1][1] && gridBoard[1][1] == gridBoard[2][0] && gridBoard[2][0] == 'O') {
+            totalWInsO = totalWInsO + 1;
+            console.log(totalWInsO + 'O');
         }
     }
 }
--- js/main.js
+++ js/main.js
@@ -9,28 +9,26 @@ let playerTurn = 'X';
 let totalWinsX = 0;
 let totalWInsO = 0;
 let totalDraws = 0;
+let didWin = false;
 
 boxes.forEach((box, index) => {
-    box.addEventListener('click', (data) => {;
-        place = gridBoard[Math.floor(index / 3)][index % 3];
-        console.log(data.target.attributes[0].nodeValue);
-        if (playerTurn === 'X') {
-            if (data.target.attributes[0].nodeValue != 'img/o.png') {
-                data.target.attributes[0].nodeValue = 'img/x.png';
-                place = gridBoard[Math.floor(index / 3)][index % 3] = playerTurn;
-                console.log(place);
-                playerTurn = 'O';
+    box.addEventListener('click', (data) => { //EventListener collects the data of the clicked box
+        place = gridBoard[Math.floor(index / 3)][index % 3]; //places the x or o on the array board
+        if (didWin == false) { //x and o can be placed as long as no one has won yet
+            if (playerTurn === 'X') {
+                if (data.target.attributes[0].nodeValue != 'img/o.png') {
+                    data.target.attributes[0].nodeValue = 'img/x.png';
+                    place = gridBoard[Math.floor(index / 3)][index % 3] = playerTurn;
+                    playerTurn = 'O';
+                }
+            } else if (playerTurn === 'O') {
+                if (data.target.attributes[0].nodeValue != 'img/x.png') {
+                    data.target.attributes[0].nodeValue = 'img/o.png';
+                    place = gridBoard[Math.floor(index / 3)][index % 3] = playerTurn;
+                    playerTurn = 'X';
+                }
             }
-        } else if (playerTurn === 'O') {
-            if (data.target.attributes[0].nodeValue != 'img/x.png') {
-                data.target.attributes[0].nodeValue = 'img/o.png';
-                place = gridBoard[Math.floor(index / 3)][index % 3] = playerTurn;
-                console.log(place);
-                playerTurn = 'X';
-            }
+            checkWinner();
         }
-
-        checkWinner();
-        console.log(gridBoard);
     })
 });
