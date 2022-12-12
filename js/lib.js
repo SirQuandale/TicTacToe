7a7bb6c0ed8f3880a14d4d5dccaf5c1bfeadd852
SirQuandale <302685723@student.rocmondriaan.nl>
2022-12-12 10:26:15 +0100

    fixed the bug that doesn't remove the green win background. Still doesn't work.

--- js/lib.js
+++ js/lib.js
@@ -2,7 +2,7 @@ function reloadData() {
     totalWinsX = parseInt(window.localStorage.getItem('totalXwins'));
 }
 
-if(window.localStorage.length != 0) {
+if (window.localStorage.length != 0) {
     reloadData();
 }
 
@@ -89,44 +89,59 @@ function addPoints() {
 }
 
 function eventReset() {
-    for(let i = 0; i < 3; i++)
-    {
-        boxes[i].attributes[0].nodeValue = 'img/blank.png';
-        didWin = false;
-        gridBoard = [
-            [null, null, null],
-            [null, null, null],
-            [null, null, null]
-        ]
-        if (gridBoard[i][0] == gridBoard[i][1] && gridBoard[i][1] == gridBoard[i][2] && gridBoard[i][2] != null) { //checks if one of these horizontal rows is filled. the [0] or [1] = rows
-            let row = document.querySelectorAll(`.row${i + 1}`); //checks all the three rows
-            row.forEach(column => { //for one of the winning rows, the background changes color to green.
-                column.classList.remove('won'); //adds a green background to the winning 3 boxes
-                column.classList.add('boxes-hover'); //removes the gray hover color
-            })
+    for (let i = 0; i < boxes.length; i++) {
+        for (let p = 1; p < boxes.length; p++){
+            didWin = false;
+            if (gridBoard[i][0] == gridBoard[i][1] && gridBoard[i][1] == gridBoard[i][2] && gridBoard[i][2] != null) { //checks if one of these horizontal rows is filled. the [0] or [1] = rows
+                let row = document.querySelectorAll(`.row${i + 1}`); //checks all the three rows
+                row.forEach(column => { //for one of the winning rows, the background changes color to green.
+                    column.classList.remove('won'); //adds a green background to the winning 3 boxes
+                    column.classList.add('boxes-hover'); //removes the gray hover color
+                })
+                row = document.querySelectorAll(`.row${i + 2}`);
+                row.forEach(column => { //for one of the winning rows, the background changes color to green.
+                    column.classList.remove('won'); //adds a green background to the winning 3 boxes
+                    column.classList.add('boxes-hover'); //removes the gray hover color
+                })
+                // return gridBoard[i][0];
+            } else if (gridBoard[0][i] == gridBoard[1][i] && gridBoard[1][i] == gridBoard[2][i] && gridBoard[2][i] != null) { //the same is repeated here, except the indexes are turned around to recognize vertical rows
+                let column = document.querySelectorAll(`.column${i + 1}`) //column instead of row
+                column.forEach(columnrow => {
+                    columnrow.classList.remove('won');
+                    columnrow.classList.add('boxes-hover');
+                })
+                // return gridBoard[i][0];
+            } else if (gridBoard[0][0] == gridBoard[1][1] && gridBoard[1][1] == gridBoard[2][2] && gridBoard[2][2] != null) { //specific indexes for diagnal win
+                let diagnl = document.querySelectorAll(`.diag1`)
+                diagnl.forEach(diagrow => {
+                    diagrow.classList.remove('won');
+                    diagrow.classList.add('boxes-hover');
+                })
+                // return gridBoard[0][0];
+            } else if (gridBoard[0][2] == gridBoard[1][1] && gridBoard[1][1] == gridBoard[2][0] && gridBoard[2][0] != null) { //diagnal win
+                let diagnl2 = document.querySelectorAll(`.diag2`)
+                diagnl2.forEach(diagrow2 => {
+                    diagrow2.classList.remove('won');
+                    diagrow2.classList.add('boxes-hover');
+                })
+                // return gridBoard[0][0];
+            }
+            gridBoard = [    
+                [null, null, null],
+                [null, null, null],
+                [null, null, null]
+            ]
+            console.log(gridBoard);
+            boxes[0].attributes[0].nodeValue = 'img/blank.png';
+            boxes[1].attributes[0].nodeValue = 'img/blank.png';
+            boxes[2].attributes[0].nodeValue = 'img/blank.png';
+            boxes[3].attributes[0].nodeValue = 'img/blank.png';
+            boxes[4].attributes[0].nodeValue = 'img/blank.png';
+            boxes[5].attributes[0].nodeValue = 'img/blank.png';
+            boxes[6].attributes[0].nodeValue = 'img/blank.png';
+            boxes[7].attributes[0].nodeValue = 'img/blank.png';
+            boxes[8].attributes[0].nodeValue = 'img/blank.png';
             return gridBoard[i][0];
-        } else if (gridBoard[0][i] == gridBoard[1][i] && gridBoard[1][i] == gridBoard[2][i] && gridBoard[2][i] != null) { //the same is repeated here, except the indexes are turned around to recognize vertical rows
-            let column = document.querySelectorAll(`.column${i + 1}`); //column instead of row
-            column.forEach(columnrow => {
-                columnrow.classList.remove('won');
-                columnrow.classList.add('boxes-hover');
-            })
-            return gridBoard[i][0];
-        } else if (gridBoard[0][0] == gridBoard[1][1] && gridBoard[1][1] == gridBoard[2][2] && gridBoard[2][2] != null) { //specific indexes for diagnal win
-            let diagnl = document.querySelectorAll(`.diag1`);
-            boxes[i].attributes[0].nodeValue = 'img/blank.png';
-            diagnl.forEach(diagrow => {
-                diagrow.classList.remove('won');
-                diagrow.classList.add('boxes-hover');
-            })
-            return gridBoard[0][0];
-        } else if (gridBoard[0][2] == gridBoard[1][1] && gridBoard[1][1] == gridBoard[2][0] && gridBoard[2][0] != null) { //diagnal win
-            let diagnl2 = document.querySelectorAll(`.diag2`);
-            diagnl2.forEach(diagrow2 => {
-                diagrow2.classList.remove('won');
-                diagrow2.classList.add('boxes-hover');
-            })
-            return gridBoard[0][0];
         }
     }
 }
@@ -138,8 +153,14 @@ function eventClearScore() {
 
 function validateForm() {
     let x = document.forms["myForm"]["fname"].value;
+    let y = document.forms['MyForm']["fname2"].value;
+    console.log(x);
+    console.log(y);
     if (x == "") {
-      alert("Name must be filled out");
-      return false;
+        alert("Name must be filled out");
+        return false;
+    } else if (y == "") {
+        alert("Name must be filled out");
+        return false;
     }
 }
