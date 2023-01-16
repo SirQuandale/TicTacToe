ab2c34da10c3905f44f7508a567dd9fccb1d3944
SirQuandale <302685723@student.rocmondriaan.nl>
2023-01-16 11:47:57 +0100

    validateForm() & open PvE

--- js/main.js
+++ js/main.js
@@ -5,9 +5,10 @@ if(submitBtn) {
 }
 
 document.querySelector('.pve-button').addEventListener('click', function () {
-    window.open('pve.html', '_self');
+    validateForm()
     x = document.querySelector('.fname').value;
     y =  document.querySelector('.fname-two').value;
     localStorage.setItem("playerOne", x);
     localStorage.setItem("playerTwo", y);
+    window.open('pve.html', '_self');
 })
